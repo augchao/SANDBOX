7c76893b9aa3d1671a394222d4b89a2ae473a5a4
August Chao <augchao@itri.org.tw>
2016-06-17 15:40:45 +0800

    fix git clone dir error

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 GIT_CMD=/usr/bin/git
-$GIT_CMD clone https://github.com/augchao/SANDBOX.git 
+WORK_HOME=/home/jovyan/work
+
+cd $WORK_HOME; $GIT_CMD clone https://github.com/augchao/SANDBOX.git
 
 /usr/local/bin/start-notebook.sh
